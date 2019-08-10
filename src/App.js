2a6dde9b35cb88f44a9315ebebeac428612e0514
David Bernhard <davidbernhard@live.com>
2019-08-10 12:17:52 +0100

    Implement GET request for search term

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 
 import { Grid } from "@material-ui/core";
-
-import { SearchBar, VideoList, VideoDetail } from "./components";
+import { SearchBar, VideoDetail } from "./components";
 
 import youtube from "./api/youtube";
+const apiKey = require("./config/keys").youtubeKey;
 
 class App extends Component {
+  handleSubmit = async searchTerm => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: apiKey,
+        q: searchTerm
+      }
+    });
+
+    console.log(response);
+  };
+
   render() {
     return (
-      <Grid justify="center" container spacing={16}>
+      <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
-          <Grid container spacing={16}>
+          <Grid container spacing={10}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
               <VideoDetail />
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-const apiKey = require("../config/keys").youtubeKey;
-
 export default axios.create({
   // axios.create() is a helper function that makes our axios.get() calls easier elsewhere
-  baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    key: apiKey
-  }
+  baseURL: "https://www.googleapis.com/youtube/v3"
 });
