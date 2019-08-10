fe10b23de89f23779efdad671edb16253162b317
David Bernhard <davidbernhard@live.com>
2019-08-10 11:34:09 +0100

    Establish app grid structure with material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
+import { Grid } from "@material-ui/core";
+
+import youtube from "./api/youtube";
+
 class App extends Component {
   render() {
-    return <h1>YouTube Clone App</h1>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* SEARCH BAR */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* VIDEO DETAILS */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* VIDEO LIST */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
