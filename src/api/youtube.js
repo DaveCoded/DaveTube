1d391891483d16040a007e679b2905b7b30601fd
David Bernhard <davidbernhard@live.com>
2019-08-10 10:41:21 +0100

    import hidden api key from config folder

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const apiKey = require("../../config/keys").youtubeKey;
+
 export default axios.create({
   // axios.create() is a helper function that makes our axios.get() calls easier elsewhere
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: "[API KEY]"
+    key: apiKey
   }
 });
